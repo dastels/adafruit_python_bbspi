a1e3f9f5bcae2b8da548b067bc90bdaf88f88753
Dave Astels <dastels@daveastels.com>
2019-01-26 01:21:06 +0000

    Tweak output format

--- bme280_bbspi_test.py
+++ bme280_bbspi_test.py
@@ -7,6 +7,6 @@ bbspi.open(cs=26, miso=20, mosi=21, sclk=16, baud=115200, flags=0)
 sensor = BME280(26)
 sensor.begin()
 
-print('Temperature: {0}\nHumidity: {1}\nPressure: {2}\n'.format(sensor.temperature, sensor.humidity, sensor.pressure))
+print('Temperature: {0:.2f}\nHumidity: {1:.2f}\nPressure: {2:.2f}\n'.format(sensor.temperature, sensor.humidity, sensor.pressure))
 
 bbspi.close(cs=26)
